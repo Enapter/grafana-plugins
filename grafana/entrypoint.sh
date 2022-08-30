1428dbffa00e01e28404a1662743dd4c5b9c3903
Roman Novatorov <rnovatorov@enapter.com>
2022-08-30 12:47:31 +0300

    grafana: entrypoint: fix error message when token not found

--- grafana/entrypoint.sh
+++ grafana/entrypoint.sh
@@ -13,10 +13,10 @@ export GF_AUTH_ANONYMOUS_ENABLED=true
 export GF_AUTH_ANONYMOUS_ORG_ROLE=Admin
 
 TELEMETRY_API_BASE_URL=${TELEMETRY_API_BASE_URL:-https://api.enapter.com/telemetry}
-TELEMETRY_API_TOKEN=${TELEMETRY_API_TOKEN:-$ENAPTER_API_TOKEN}
+TELEMETRY_API_TOKEN=${TELEMETRY_API_TOKEN:-${ENAPTER_API_TOKEN:-}}
 
 if [ -z "$TELEMETRY_API_TOKEN" ]; then
-	echo "both TELEMETRY_API_TOKEN and ENAPTER_API_TOKEN are empty or missing" > /dev/stderr
+	echo "Both TELEMETRY_API_TOKEN and ENAPTER_API_TOKEN are empty or missing." > /dev/stderr
 	exit 1
 fi
 
